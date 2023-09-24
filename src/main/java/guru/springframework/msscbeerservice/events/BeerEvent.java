0d094d7fc59a3be0b26ec748ba1378106b76aa90
cevher.bozkur <bozkur@gmail.com>
2023-09-24 09:04:57 +0300

    Added default constructor to event classes. Event object could not be deserialized because related classes lacks default constructors. Default constructor is added to event classes.

--- src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
+++ src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
@@ -1,9 +1,10 @@
 package guru.springframework.msscbeerservice.events;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -12,10 +13,11 @@ import java.io.Serializable;
  * @author cevher
  */
 @Data
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class BeerEvent implements Serializable {
     @Serial
     private static final long serialVersionUID = 3738178240496566665L;
-    private final BeerDto beerDto;
+    private BeerDto beerDto;
 }
--- src/main/java/guru/springframework/msscbeerservice/events/BrewBeerEvent.java
+++ src/main/java/guru/springframework/msscbeerservice/events/BrewBeerEvent.java
@@ -1,12 +1,14 @@
 package guru.springframework.msscbeerservice.events;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.AllArgsConstructor;
 
 import java.io.Serial;
 
 /**
  * @author cevher
  */
+@AllArgsConstructor
 public class BrewBeerEvent extends BeerEvent{
     @Serial
     private static final long serialVersionUID = 938264382931294419L;
