d0ea9e3a79ff24fa214c1415729a11d58e45cb44
cevher.bozkur <bozkur@gmail.com>
2023-09-24 09:06:38 +0300

    ObjectMapper supporting OffsetDateTime converter is injected to message converter.

--- src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
+++ src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
@@ -1,5 +1,6 @@
 package guru.springframework.msscbeerservice.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
@@ -16,10 +17,11 @@ public class JmsConfig {
     public static final String NEW_INVENTORY_QUEUE = "new-inventory";
 
     @Bean
-    public MessageConverter messageConverter() {
+    public MessageConverter messageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
         messageConverter.setTypeIdPropertyName("_type");
         messageConverter.setTargetType(MessageType.TEXT);
+        messageConverter.setObjectMapper(objectMapper);
         return messageConverter;
     }
 }
